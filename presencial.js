b6beb1543bb97079ae137a35dfbf2e2890281e8e
Groove Tech <felipe.ribeiro@hubchain.io>
2022-03-07 19:29:36 -0300

    presencial1

---
+++ presencial.js
@@ -0,0 +1,25 @@
+const douglas = [
+
+    {
+        objeto1: [
+            fruta = "banana",
+            quantidade = 12,
+            estragado = false,
+        ],
+        objeto2:
+            [
+                fruta = "morango",
+                quantidade = 10,
+                estragado = false
+            ],
+            objeto3:
+            [
+                fruta = "morango",
+                quantidade = 10,
+                estragado = false
+            ],
+
+
+
+    }
+]
