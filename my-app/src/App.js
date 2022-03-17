5e6e23391ecb2e236b077b84a6919db226753e72
Groove Tech <felipe.ribeiro@hubchain.io>
2022-03-17 18:11:04 -0300

    aula 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,72 +2,78 @@ import './App.css';
 
 function App() {
 
-  const familia = [
+  const futebol = [
     {
-      nome: "Douglas",
-      idade: 28,
-      dataDeNascimento: "29/01/1994",
-      telefone: "(11) 93023-4589",
-      nomeDaMae: "Edileide",
-      nomeDoPai: "Cicero",
+      nome: "Cristiano Ronaldo",
+      idade: 37,
+      time: "Real Madrid",
+      melhorDoMundo: true
     },
 
     {
-      nome: "Diego",
-      idade: 31,
-      dataDeNascimento: "25/08/1990",
-      telefone: "(11) 95566-7788",
-      nomeDaMae: "Edileuza",
-      nomeDoPai: "",
+      nome: "Toni Kross",
+      idade: 32,
+      time: "Real Madrid",
+      melhorDoMundo: false
     },
 
     {
-      nome: "William",
-      idade: 30,
-      dataDeNascimento: "10/04/1990",
-      telefone: "(11) 99876-5432",
-      nomeDaMae: "Neide",
-      nomeDoPai: "Sidney",
+      nome: "Lionel Messi",
+      idade: 34,
+      time: "Paris Saint-Germain",
+      melhorDoMundo: true
     },
 
     {
-      nome: "Patrick",
-      idade: 27,
-      dataDeNascimento: "23/07/1996",
-      telefone: "(11) 91234-5678",
-      nomeDaMae: "Maria",
-      nomeDoPai: "",
+      nome: "Renato Augusto",
+      idade: 34,
+      time: "Corinthians",
+      melhorDoMundo: false
+    }
+  ]
+
+  const comida = [
+
+    {
+      nome: "Cristiano Ronaldo",
+      idade: 37,
+      time: "Real Madrid",
+      melhorDoMundo: true
     },
 
     {
-      nome: "Wilker",
-      idade: 26,
-      dataDeNascimento: "22/06/1995",
-      telefone: "(11) 91020-3040",
-      nomeDaMae: "Neide",
-      nomeDoPai: "Sidney",
+      nome: "Toni Kross",
+      idade: 32,
+      time: "Real Madrid",
+      melhorDoMundo: false
     },
+
+    {
+      nome: "Lionel Messi",
+      idade: 34,
+      time: "Paris Saint-Germain",
+      melhorDoMundo: true
+    },
+
+
 
   ]
 
-
+  const filtrando = futebol.filter(jogadores => {
+    return jogadores.melhorDoMundo === false
+  })
 
   return (
 
     <div className="App">
       {
-        familia.map(item => {
+        filtrando.map(jogadores => {
 
           return (
             <div className="box">
-              <p> Nome: {item.nome} </p>
-              <p> Idade: {item.idade}</p>
-              <p> Data de nascimento:{item.dataDeNascimento}</p>
-              <p> Telefone: {item.telefone}</p>
-              <p> Nome da Mãe: {item.nomeDaMae}</p>
-              {item.nomeDoPai &&
-                <p> Nome do Pai:{item.nomeDoPai}</p>
-              }
+              <p> Nome: {jogadores.nome} </p>
+              <p> Idade: {jogadores.idade}</p>
+              <p>Time:{jogadores.time}</p>
             </div>
           )
         })
