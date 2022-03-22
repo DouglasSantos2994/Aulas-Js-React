dcd0087704e560ec5bbb65c6fb627fc665b273b1
Groove Tech <felipe.ribeiro@hubchain.io>
2022-03-22 09:28:07 -0300

    presencial/4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,77 +2,77 @@ import './App.css';
 
 function App() {
 
-  const familia = [
+  const modalidades = [
     {
-      nome: "Douglas",
-      idade: 28,
-      dataDeNascimento: "29/01/1994",
-      telefone: "(11) 93023-4589",
-      nomeDaMae: "Edileide",
-      nomeDoPai: "Cicero",
+      nome: "Messi",
+      time: "Paris Saint Germain",
+      nacionalidade: "Argentino",
+      melhorEsporte: [
+        {
+          esporte: "Futebol",
+          local: "Campo",
+
+        },
+
+        {
+          esporte: "Basquete",
+          local: "Quadra",
+        },
+
+      ],
     },
 
     {
-      nome: "Diego",
-      idade: 31,
-      dataDeNascimento: "25/08/1990",
-      telefone: "(11) 95566-7788",
-      nomeDaMae: "Edileuza",
-      nomeDoPai: "",
-    },
+      nome: "Stephen curry",
+      time: "Golden State Warriors",
+      nacionalidade: "Americano",
+      melhorEsporte: [
+        {
+          esporte: "Basquete",
+          local: "Quadra",
+        },
 
-    {
-      nome: "William",
-      idade: 30,
-      dataDeNascimento: "10/04/1990",
-      telefone: "(11) 99876-5432",
-      nomeDaMae: "Neide",
-      nomeDoPai: "Sidney",
+        {
+          esporte: "Futebol",
+          local: "Campo",
+        },
+      ],
     },
-
-    {
-      nome: "Patrick",
-      idade: 27,
-      dataDeNascimento: "23/07/1996",
-      telefone: "(11) 91234-5678",
-      nomeDaMae: "Maria",
-      nomeDoPai: "",
-    },
-
-    {
-      nome: "Wilker",
-      idade: 26,
-      dataDeNascimento: "22/06/1995",
-      telefone: "(11) 91020-3040",
-      nomeDaMae: "Neide",
-      nomeDoPai: "Sidney",
-    },
-
   ]
-
 
 
   return (
 
     <div className="App">
       {
-        familia.map(item => {
+        modalidades.map(jogadores => {
+
+          const fifa = jogadores.melhorEsporte.filter(jogador => {
+            return jogador.esporte != "Futebol"
+          })
 
           return (
             <div className="box">
-              <p> Nome: {item.nome} </p>
-              <p> Idade: {item.idade}</p>
-              <p> Data de nascimento:{item.dataDeNascimento}</p>
-              <p> Telefone: {item.telefone}</p>
-              <p> Nome da Mãe: {item.nomeDaMae}</p>
-              {item.nomeDoPai &&
-                <p> Nome do Pai:{item.nomeDoPai}</p>
-              }
+              <p> Nome: {jogadores.nome} </p>
+              <p>Time: {jogadores.time}</p>
+              <p>Nacionalidade: {jogadores.nacionalidade}</p>
+
+              {fifa.map(modalidade => {
+
+                return (
+                  <>
+                    <p>Esporte: {modalidade.esporte}</p>
+                    <p>Local: {modalidade.local}</p>
+                  </>
+                )
+              })}
+
             </div>
           )
         })
       }
 
+     
     </div>
   );
 }
