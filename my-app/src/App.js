1ffbfdf661acf0f8443ed0ddc462fed4fe8dcbb3
Felipe <felipe.ribeiro@hubchain.com>
2022-05-09 09:11:58 -0300

    presencial 10

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,176 +1,139 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [topFilmes, setTopFilmes] = useState();
 
-  const brasil = {
-    clube: 'Brasil',
-    time: [
-      { jogador: 'Alisson ' },
-      { jogador: 'Daniel Alves' },
-      { jogador: 'Marquinhos' },
-      { jogador: 'Thiago Silva' },
-      { jogador: 'Guilherme Arana' },
-      { jogador: 'Casemiro' },
-      { jogador: 'Btuno Guimarães' },
-      { jogador: 'Lucas Paquetá' },
-      { jogador: 'Neymar' },
-      { jogador: 'Matheus Cunha' },
-      { jogador: 'Vinicius Junior' }
-    ]
-  }
-
-  const corinthians = {
-    clube: 'Corinthians',
-    time: [
-      { jogador: 'Cássio ' },
-      { jogador: 'Fagner' },
-      { jogador: 'Gil' },
-      { jogador: 'João Victor' },
-      { jogador: 'Fabio Santos' },
-      { jogador: 'Maycon' },
-      { jogador: 'Giuliano' },
-      { jogador: 'Renato Augusto' },
-      { jogador: 'Róger Guedes' },
-      { jogador: 'Willian' },
-      { jogador: 'Jô' }
-    ]
-  }
-
-  const palmeiras = {
-    clube: 'Palmeiras',
-    time: [
-      { jogador: 'Weverton' },
-      { jogador: 'Mayke' },
-      { jogador: 'Kuscevic' },
-      { jogador: 'Gustavo Gómes' },
-      { jogador: 'Jorge' },
-      { jogador: 'Danilo' },
-      { jogador: 'Atuesta' },
-      { jogador: 'Gustavo Scarpa' },
-      { jogador: 'Dudu' },
-      { jogador: 'Rony' },
-      { jogador: 'Rafael Navarro' }
-    ]
-  }
-
-  const saoPaulo = {
-    clube: 'São Paulo',
-    time: [
-      { jogador: 'Tiago Volpi' },
-      { jogador: 'Igor Vinicius' },
-      { jogador: 'Arboleda' },
-      { jogador: 'Leo' },
-      { jogador: 'Reinaldo' },
-      { jogador: 'Luan' },
-      { jogador: 'Igor Gomes' },
-      { jogador: 'Gabriel Sara' },
-      { jogador: 'Alisson' },
-      { jogador: 'Eder' },
-      { jogador: 'Luciano' }
-    ]
-  }
-
-  const celulas = {
-    celula: [
-      { membro: 'Douglas' },
-      { membro: 'Leyd' },
-      { membro: 'Stephanie' },
-      { membro: 'Léia' },
-      { membro: 'Mayara' },
-      { membro: 'Bianca' },
-      { membro: 'Marcelo' },
-      { membro: 'Diego' },
-      { membro: 'Jaqueline' },
-      { membro: 'Pamela' },
-    ],
-    celulaBruno: [
-      { membro: 'Bruno' },
-      { membro: 'Sandy' },
-      { membro: 'Sarinha' },
-      { membro: 'Lari' },
-      { membro: 'Elisa' },
-      { membro: 'Taci' },
-      { membro: 'Lisandra' },
-      { membro: 'Matheus' },
-      { membro: 'Raquel' },
-      { membro: 'Rodrigada' },
-    ],
-    celulaFelipe: [
-      { membro: 'Felipe' },
-      { membro: 'Prisciane' },
-      { membro: 'Vitão' },
-      { membro: 'Italo' },
-      { membro: 'Bia' },
-      { membro: 'Caio' },
-      { membro: 'Grazy' },
-      { membro: 'Julia' },
-      { membro: 'Nathan' },
-      { membro: 'Mari' },
-    ]
-  }
-
-  const [substituir, setsubstituir] = useState(0)
-  const [clubes, setClubes] = useState(brasil.clube)
-  const [time, setTime] = useState(brasil.time)
-
-  const [alterarCelula, setAlterarCelula] = useState(0)
-  const [familia, setFamilia] = useState(celulas.celula)
+  const [informacoes, setInformacoes] = useState({
+    titulo: "",
+    descricao: "",
+    diretor: "",
+    genero: "",
+    personagens: "",
+    video: "",
+    imagens: "",
+  });
 
   useEffect(() => {
-
-    if (substituir === 1) {
-      setClubes(corinthians.clube)
-      setTime(corinthians.time)
+    if (topFilmes === "filme1") {
+      setInformacoes({
+        titulo: "Velozes e Furiosos",
+        descricao: "",
+        diretor: "",
+        genero: "",
+        personagens: "",
+        video: "",
+        imagens: "",
+      });
+    } else if (topFilmes === "filme2") {
+      setInformacoes({
+        titulo: "Viuva Negra",
+        descricao: "",
+        diretor: "",
+        genero: "",
+        personagens: "",
+        video: "",
+        imagens: "",
+      });
+    } else if (topFilmes === "filme3") {
+      setInformacoes({
+        titulo: "Harry Potter",
+        descricao: "",
+        diretor: "",
+        genero: "",
+        personagens: "",
+        video: "",
+        imagens: "",
+      });
+    } else if (topFilmes === "filme4") {
+      setInformacoes({
+        titulo: "Crepúsculo",
+        descricao: "",
+        diretor: "Catherine Hardwicke",
+        genero: "fantasia, romance",
+        personagens: "Bella Swan, Edward Cullen, Jacob Black",
+        video: "",
+        imagens: "",
+      });
+    } else if (topFilmes === "filme5") {
+      setInformacoes({
+        titulo: "Coringa",
+        descricao: "",
+        diretor: "",
+        genero: "",
+        personagens: "",
+        video: "",
+        imagens: "",
+      });
+    } else if (topFilmes === "filme6") {
+      setInformacoes({
+        titulo: "Homem-Aranha",
+        descricao: "",
+        diretor: "",
+        genero: "",
+        personagens: "",
+        video: "",
+        imagens: "",
+      });
     }
-
-    if (substituir === 2) {
-      setClubes(palmeiras.clube)
-      setTime(palmeiras.time)
-    }
-
-    if (substituir === 3) {
-      setClubes(saoPaulo.clube)
-      setTime(saoPaulo.time)
-    }
-
-
-  }, [substituir])
-
-  useEffect(() => {
-    if (alterarCelula === 0) {
-      setFamilia(celulas.celula)
-    } else if (alterarCelula === 1) {
-      setFamilia(celulas.celulaBruno)
-    } else if (alterarCelula === 2) {
-      setFamilia(celulas.celulaFelipe)
-    } else {
-      setAlterarCelula(0)
-    }
-  }, [alterarCelula])
+  }, [topFilmes]);
 
   return (
     <div className="container">
-
-      <div className='box'>
-        <h1>{clubes}</h1>
-        {time.map(jogadores => (
-          <p>{jogadores.jogador}</p>
-        ))}
+      <div>
+        <button onClick={() => setTopFilmes("filme1")}>
+          <img src="https://media.fstatic.com/X_AzJVc9TjBw_LphxSQKaRz-Sx8=/290x478/smart/media/movies/covers/2021/06/images_3_18H0te3.jpeg" />
+        </button>
       </div>
 
-      <div className='botoes'>
-        <button onClick={() => setsubstituir(1)}>Corinthians</button>
-        <button onClick={() => setsubstituir(2)}>Palmeiras </button>
-        <button onClick={() => setsubstituir(3)}>São Paulo</button>
+      <div>
+        <button onClick={() => setTopFilmes("filme2")}>
+          <img
+            className="filmes"
+            src="https://br.web.img3.acsta.net/c_310_420/pictures/20/03/09/15/51/4538015.jpg"
+          />
+        </button>
       </div>
 
-      <div className='box'>
-        {familia.map(nome => (
-          <p>{nome.membro}</p>
-        ))}
+      <div>
+        <button onClick={() => setTopFilmes("filme3")}>
+          <img
+            className="filmes"
+            src="https://veja.abril.com.br/wp-content/uploads/2016/06/harry-potter-cartaz-calice-fogo-04-original1.jpeg?quality=70&strip=info&w=395"
+          />
+        </button>
+      </div>
 
-        <button onClick={() => setAlterarCelula(alterarCelula +1)}>Alterar celula</button>
+      <div>
+        <button onClick={() => setTopFilmes("filme4")}>
+          <img
+            className="filmes"
+            src="https://cdn.ome.lt/vZvPvwbitCjuqVOgmUo2e4s__ZE=/fit-in/837x500/smart/uploads/conteudo/fotos/amanhecer-2_5.jpg"
+          />
+        </button>
+      </div>
+
+      <div>
+        <button onClick={() => setTopFilmes("filme5")}>
+          <img
+            className="filmes"
+            src="https://upload.wikimedia.org/wikipedia/pt/thumb/6/63/Joker_%282019%29.jpg/250px-Joker_%282019%29.jpg"
+          />
+        </button>
+      </div>
+
+      <div>
+        <button onClick={() => setTopFilmes("filme6")}>
+          <img
+            className="filmes"
+            src="https://timnews.com.br/tagger/images?url=https%3A%2F%2Fportalpopline.com.br%2Fwp-content%2Fuploads%2F2021%2F11%2Fhomem-aranha-trailer-640x800.jpg"
+          />
+        </button>
+      </div>
+
+      <div className="infomacao">
+        <h1>{informacoes.titulo}</h1>
+        <p>{informacoes.personagens}</p>
       </div>
     </div>
   );
