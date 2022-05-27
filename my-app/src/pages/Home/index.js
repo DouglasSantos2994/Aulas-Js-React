69c1ac755c931d0480cf13c127d7977a8e986917
Felipe <felipe.ribeiro@hubchain.com>
2022-05-27 09:44:36 -0300

    exclusao de console

--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Home = () => {
   const [resposta, setResposta] = useState();
-  console.log(resposta);
+
   useEffect(() => {
     axios
       .get("https://aula-metodos-default-rtdb.firebaseio.com/futebol.json")
@@ -16,16 +16,15 @@ const Home = () => {
     <>
       {resposta &&
         Object.values(resposta?.clubes).map((futebol, index) => {
-          console.log("fut", futebol);
           return (
-            <divclass key={index}>
+            <div key={index}>
               <div className="box">
                 <p>Clube: {futebol.time} </p>
                 <img src={futebol.imagem} />
                 <p>Jogador: {futebol.nome}</p>
                 <p>Posição: {futebol.posicao}</p>
               </div>
-            </divclass>
+            </div>
           );
         })}
     </>
