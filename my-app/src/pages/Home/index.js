5fc44e00064cba859ab80436cbc78d7f1130b6a4
Felipe <felipe.ribeiro@hubchain.com>
2022-06-06 08:28:49 -0300

    aula/14

--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -3,7 +3,11 @@ import axios from "axios";
 
 const Home = () => {
   const [resposta, setResposta] = useState();
-  console.log(resposta);
+  const [time, setTime] = useState();
+  const [nome, setNome] = useState();
+  const [posicao, setPosicao] = useState();
+  const [mapear, setMaper] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://aula-metodos-default-rtdb.firebaseio.com/futebol.json")
@@ -12,20 +16,107 @@ const Home = () => {
       });
   }, []);
 
+  const cadastrar = () => {
+    axios
+      .post(
+        "https://aula-metodos-default-rtdb.firebaseio.com/futebol/clubes.json",
+        { time: time, nome: nome, posicao: posicao }
+      )
+      .then(() => {
+        alert("seu time foi cadastrado");
+        setMaper(!mapear);
+      })
+      .catch(() => alert("não conseguiu cadastrar esse time"));
+  };
+
+  const deletar = (id) => {
+    axios
+      .delete(
+        `https://aula-metodos-default-rtdb.firebaseio.com/futebol/clubes/${id}.json`,
+        {}
+      )
+      .then(() => {
+        alert("seu clube foi excluido");
+        setMaper(!mapear);
+      })
+      .catch(() => alert("não foi possivel excluir esse time"));
+  };
+
+  const editar = (id) => {
+    axios
+      .patch(
+        `https://aula-metodos-default-rtdb.firebaseio.com/futebol/clubes/${id}.json`,
+        { time: time, nome: nome, posicao: posicao }
+      )
+      .then(() => {
+        alert("Seu time foi editado!");
+        setMaper(!mapear);
+      })
+      .catch(() => {
+        alert("Não foi possivel editar esse time");
+      });
+  };
+
   return (
     <>
+      <h2>Cadastramento de jogadores</h2>
+      <form>
+        <label>Time</label>
+        <input onChange={(e) => setTime(e.target.value)} />
+        <br />
+        <label>Jogador</label>
+        <input onChange={(e) => setNome(e.target.value)} />
+        <br />
+        <label>Posição</label>
+        <input onChange={(e) => setPosicao(e.target.value)} />
+        <br />
+        <br />
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            cadastrar();
+          }}
+        >
+          Cadastrar
+        </button>
+      </form>
+
       {resposta &&
-        Object.values(resposta?.clubes).map((futebol, index) => {
-          console.log("fut", futebol);
+        Object.entries(resposta?.clubes).map((futebol, index) => {
           return (
-            <divclass key={index}>
+            <div key={index}>
               <div className="box">
-                <p>Clube: {futebol.time} </p>
-                <img src={futebol.imagem} />
-                <p>Jogador: {futebol.nome}</p>
-                <p>Posição: {futebol.posicao}</p>
+                <p>Clube: {futebol[1].time} </p>
+                <input
+                  onChange={(e) => {
+                    setTime(e.target.value);
+                  }}
+                />
+
+                <button onClick={() => editar(futebol[0])}>Editar</button>
+
+                <p>Jogador: {futebol[1].nome}</p>
+                <input
+                  onChange={(e) => {
+                    setNome(e.target.value);
+                  }}
+                />
+
+                <button onClick={() => editar(futebol[0])}>Editar</button>
+
+                <p>Posição: {futebol[1].posicao}</p>
+                <input
+                  onChange={(e) => {
+                    setPosicao(e.target.value);
+                  }}
+                />
+
+                <button onClick={() => editar(futebol[0])}>Editar</button>
+                <br />
+                <button onClick={() => deletar(futebol[0])}>Excluir</button>
               </div>
-            </divclass>
+            </div>
           );
         })}
     </>
