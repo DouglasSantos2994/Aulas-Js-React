81eceb5a276f62256dcb229eb4325c2e2ab4f6d2
Felipe <felipe.ribeiro@hubchain.com>
2022-06-06 21:12:25 -0300

    presencial/14

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Cadastro from "./pages/Cadastro";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
   );
--- my-app/src/pages/Home/index.js
+++ my-app/src/pages/Home/index.js
@@ -3,118 +3,55 @@ import axios from "axios";
 
 const Home = () => {
   const [resposta, setResposta] = useState();
-  const [time, setTime] = useState();
-  const [nome, setNome] = useState();
-  const [posicao, setPosicao] = useState();
+
   const [mapear, setMaper] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://aula-metodos-default-rtdb.firebaseio.com/futebol.json")
+      .get("https://aula-14-133cc-default-rtdb.firebaseio.com/.json")
       .then(function (response) {
         setResposta(response.data);
       });
-  }, []);
+  }, [mapear]);
 
-  const cadastrar = () => {
-    axios
-      .post(
-        "https://aula-metodos-default-rtdb.firebaseio.com/futebol/clubes.json",
-        { time: time, nome: nome, posicao: posicao }
-      )
-      .then(() => {
-        alert("seu time foi cadastrado");
-        setMaper(!mapear);
-      })
-      .catch(() => alert("não conseguiu cadastrar esse time"));
-  };
+  const filmes =
+    resposta &&
+    Object.values(resposta?.catalogo).filter((item) => {
+      console.log("item", item);
+      return item.tipo === "Filmes";
+    });
 
-  const deletar = (id) => {
-    axios
-      .delete(
-        `https://aula-metodos-default-rtdb.firebaseio.com/futebol/clubes/${id}.json`,
-        {}
-      )
-      .then(() => {
-        alert("seu clube foi excluido");
-        setMaper(!mapear);
-      })
-      .catch(() => alert("não foi possivel excluir esse time"));
-  };
-
-  const editar = (id) => {
-    axios
-      .patch(
-        `https://aula-metodos-default-rtdb.firebaseio.com/futebol/clubes/${id}.json`,
-        { time: time, nome: nome, posicao: posicao }
-      )
-      .then(() => {
-        alert("Seu time foi editado!");
-        setMaper(!mapear);
-      })
-      .catch(() => {
-        alert("Não foi possivel editar esse time");
-      });
-  };
+  const series =
+    resposta &&
+    Object.values(resposta?.catalogo).filter((item) => {
+      return item.tipo === "series";
+    });
 
   return (
     <>
-      <h2>Cadastramento de jogadores</h2>
-      <form>
-        <label>Time</label>
-        <input onChange={(e) => setTime(e.target.value)} />
-        <br />
-        <label>Jogador</label>
-        <input onChange={(e) => setNome(e.target.value)} />
-        <br />
-        <label>Posição</label>
-        <input onChange={(e) => setPosicao(e.target.value)} />
-        <br />
-        <br />
-
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            cadastrar();
-          }}
-        >
-          Cadastrar
-        </button>
-      </form>
-
-      {resposta &&
-        Object.entries(resposta?.clubes).map((futebol, index) => {
+      <h2>Filmes</h2>
+      {filmes &&
+        Object.entries(filmes).map((assistir, index) => {
           return (
             <div key={index}>
               <div className="box">
-                <p>Clube: {futebol[1].time} </p>
-                <input
-                  onChange={(e) => {
-                    setTime(e.target.value);
-                  }}
-                />
+                <p> {assistir[1].titulo} </p>
+                <p> {assistir[1].genero} </p>
+                <img width="100" src={assistir[1].imagem} />
+              </div>
+            </div>
+          );
+        })}
 
-                <button onClick={() => editar(futebol[0])}>Editar</button>
-
-                <p>Jogador: {futebol[1].nome}</p>
-                <input
-                  onChange={(e) => {
-                    setNome(e.target.value);
-                  }}
-                />
-
-                <button onClick={() => editar(futebol[0])}>Editar</button>
-
-                <p>Posição: {futebol[1].posicao}</p>
-                <input
-                  onChange={(e) => {
-                    setPosicao(e.target.value);
-                  }}
-                />
-
-                <button onClick={() => editar(futebol[0])}>Editar</button>
-                <br />
-                <button onClick={() => deletar(futebol[0])}>Excluir</button>
+      <h2>Séries</h2>
+      {series &&
+        Object.entries(series).map((assistir, index) => {
+          return (
+            <div key={index}>
+              <div className="box">
+                <p> {assistir[1].titulo} </p>
+                <p> {assistir[1].genero} </p>
+                <img width="100" src={assistir[1].imagem} />
               </div>
             </div>
           );
